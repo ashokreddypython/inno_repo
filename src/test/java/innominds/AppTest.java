e9b5889443355bc4559b0b617bedb717cdf7b3ee
agangula <agangula@innominds.com>
2021-11-08 16:54:33 +0000

    Server setup

---
+++ src/test/java/innominds/AppTest.java
@@ -0,0 +1,20 @@
+package innominds;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
